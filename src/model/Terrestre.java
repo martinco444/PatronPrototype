22bf72a01452334f37509f9a22e9c02dced3c0d6
Martín Correa <comartin910@gmail.com>
2022-09-21 11:15:24 -0500

    Inicio

---
+++ src/model/Terrestre.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Terrestre extends MedioTransporte {
+
+    public Terrestre(TipoAlimentacion alimentacion, String marca, String modelo, int cantidadPasajeros) {
+        super(alimentacion, marca, modelo, cantidadPasajeros);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
